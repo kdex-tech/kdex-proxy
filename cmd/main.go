0871e5221e88d939247ad26ed318f9871bf85eec
Raymond Augé <rayauge@kdex.dev>
2025-01-31 23:12:45 -0500

    use a wrapper to store the status code of the response in the middleware so we can log it

--- cmd/main.go
+++ cmd/main.go
@@ -85,13 +85,25 @@ func main() {
 	log.Println("Server stopped gracefully.")
 }
 
+type statusRecorder struct {
+	http.ResponseWriter
+	status int
+}
+
+func (rec *statusRecorder) WriteHeader(code int) {
+	rec.status = code
+	rec.ResponseWriter.WriteHeader(code)
+}
+
 func middlewareLogger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		wrapper := statusRecorder{ResponseWriter: w}
+
 		start := time.Now()
 		defer func() {
-			log.Printf("%s %s processed in %v", r.Method, r.URL.Path, time.Since(start))
+			log.Printf("%s %s status %d, processed in %v", r.Method, r.URL.Path, wrapper.status, time.Since(start))
 		}()
 
-		next.ServeHTTP(w, r)
+		next.ServeHTTP(&wrapper, r)
 	})
 }
