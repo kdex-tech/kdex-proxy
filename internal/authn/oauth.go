cab6e2bc08043c22b48292615f1e2ae62c139761
Raymond Augé <rayauge@kdex.dev>
2025-02-07 23:21:46 -0500

    back channel logout and use token sid as sessionID

--- internal/authn/oauth.go
+++ internal/authn/oauth.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"net/http"
 	"net/url"
-	"time"
 
 	"github.com/coreos/go-oidc/v3/oidc"
 	"golang.org/x/oauth2"
@@ -35,16 +34,6 @@ type OAuthValidator struct {
 	SessionStore session.SessionStore
 	StateStore   state.StateStore
 	Verifier     *oidc.IDTokenVerifier
-}
-
-type OIDCClaims struct {
-	Email    string `json:"email"`
-	Username string `json:"preferred_username"`
-}
-
-type UserData struct {
-	Claims  map[string]interface{}
-	Session *session.SessionData
 }
 
 func NewOAuthValidator(ctx context.Context, config *Config) *OAuthValidator {
@@ -93,7 +82,9 @@ func NewOAuthValidator(ctx context.Context, config *Config) *OAuthValidator {
 
 func (v *OAuthValidator) Register(mux *http.ServeMux) {
 	mux.HandleFunc("GET "+v.Config.Prefix+"oauth/callback", v.callbackHandler())
-	mux.HandleFunc("GET "+v.Config.Prefix+"oauth/signin", v.signInHandler())
+	mux.HandleFunc("GET "+v.Config.Prefix+"oauth/login", v.logInHandler())
+	mux.HandleFunc("GET "+v.Config.Prefix+"oauth/logout", v.logOutHandler())
+	mux.HandleFunc("POST "+v.Config.Prefix+"oauth/back_channel_logout", v.backChannelLogOutHandler())
 }
 
 func (v *OAuthValidator) Validate(w http.ResponseWriter, r *http.Request) func(h http.Handler) {
@@ -116,7 +107,7 @@ func (v *OAuthValidator) Validate(w http.ResponseWriter, r *http.Request) func(h
 		}
 	}
 
-	token, err := v.Provider.Verifier(&oidc.Config{
+	_, err = v.Provider.Verifier(&oidc.Config{
 		SkipClientIDCheck: true, // ?
 	}).Verify(r.Context(), sessionData.AccessToken)
 
@@ -132,24 +123,8 @@ func (v *OAuthValidator) Validate(w http.ResponseWriter, r *http.Request) func(h
 		}
 	}
 
-	var claims map[string]interface{}
-	if err := token.Claims(&claims); err != nil {
-		return func(h http.Handler) {
-			v.SessionStore.Delete(r.Context(), sessionCookie.Value)
-			http.SetCookie(w, &http.Cookie{
-				Name:  "session_id",
-				Value: "",
-				Path:  "/",
-			})
-			v.challengeAction(w, r)
-		}
-	}
-
 	return func(h http.Handler) {
-		r = r.WithContext(context.WithValue(r.Context(), ContextUserKey, UserData{
-			Claims:  claims,
-			Session: sessionData,
-		}))
+		r = r.WithContext(context.WithValue(r.Context(), ContextUserKey, sessionData))
 
 		h.ServeHTTP(w, r)
 	}
@@ -180,24 +155,21 @@ func (v *OAuthValidator) callbackHandler() http.HandlerFunc {
 			return
 		}
 
-		userInfo, err := v.validateAndGetClaimsIDToken(r.Context(), oauth2Token)
+		tokenClaims, err := v.validateAndGetClaimsIDToken(r.Context(), oauth2Token)
 		if err != nil {
 			log.Printf("Error validating and getting claims ID token: %v", err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		log.Printf("User info: %+v", userInfo)
-
-		sessionID := util.RandStringBytes(32)
+		sessionID := (*tokenClaims)["sid"].(string)
+		createdAt := util.TimeFromFloat64Seconds((*tokenClaims)["iat"].(float64))
 
 		sessionData := session.SessionData{
-			AccessToken: oauth2Token.AccessToken,
-			UserInfo: session.UserInfo{
-				Username: userInfo.Username,
-				Email:    userInfo.Email,
-			},
-			CreatedAt: time.Now(),
+			AccessToken:  oauth2Token.AccessToken,
+			CreatedAt:    createdAt,
+			Data:         *tokenClaims,
+			RefreshToken: oauth2Token.RefreshToken,
 		}
 
 		if err := v.SessionStore.Set(r.Context(), sessionID, sessionData); err != nil {
@@ -226,48 +198,13 @@ func (v *OAuthValidator) callbackHandler() http.HandlerFunc {
 
 func (v *OAuthValidator) challengeAction(w http.ResponseWriter, r *http.Request) {
 	if v.Config.SignInOnChallenge {
-		v.signInHandler()(w, r)
+		v.logInHandler()(w, r)
 	} else {
 		http.Error(w, "not found", http.StatusNotFound)
 	}
 }
 
-func (v *OAuthValidator) validateAndGetClaimsIDToken(ctx context.Context, oauth2Token *oauth2.Token) (*OIDCClaims, error) {
-	rawIDToken, ok := oauth2Token.Extra("id_token").(string)
-	if !ok {
-		return nil, fmt.Errorf("id_token is required")
-	}
-	idToken, err := v.Verifier.Verify(ctx, rawIDToken)
-	if err != nil {
-		return nil, fmt.Errorf("failed to verify id_token: %w", err)
-	}
-
-	if v.Config.DumpClaims {
-		dumpClaims := map[string]interface{}{}
-		if err := idToken.Claims(&dumpClaims); err != nil {
-			return nil, fmt.Errorf("failed to get user info: %w", err)
-		}
-		log.Printf("Dump claims: %+v", dumpClaims)
-	}
-
-	claims := OIDCClaims{}
-	if err := idToken.Claims(&claims); err != nil {
-		return nil, fmt.Errorf("failed to get user info: %w", err)
-	}
-	return &claims, nil
-}
-
-func (v *OAuthValidator) verifyState(r *http.Request) error {
-	state := r.URL.Query().Get("state")
-
-	if _, err := v.StateStore.Get(r.Context(), state); err != nil {
-		return fmt.Errorf("invalid state: %v", err)
-	}
-
-	return nil
-}
-
-func (v *OAuthValidator) signInHandler() http.HandlerFunc {
+func (v *OAuthValidator) logInHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		redirectURL := fmt.Sprintf("%s://%s%s", util.GetScheme(r), r.Host, v.Config.Prefix+"oauth/callback")
 		log.Printf("Redirect URL: %s", redirectURL)
@@ -284,4 +221,88 @@ func (v *OAuthValidator) signInHandler() http.HandlerFunc {
 		log.Printf("Auth URL: %s", authURL)
 		http.Redirect(w, r, authURL, http.StatusTemporaryRedirect)
 	}
+}
+
+func (v *OAuthValidator) logOutHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		logoutURL := fmt.Sprintf(
+			"%s/realms/%s/protocol/openid-connect/logout?redirect_uri=%s",
+			v.Config.AuthServerURL,
+			url.PathEscape(v.Config.Realm),
+			url.QueryEscape(v.Config.RedirectURI),
+		)
+		log.Printf("Logout URL: %s", logoutURL)
+		http.Redirect(w, r, logoutURL, http.StatusTemporaryRedirect)
+	}
+}
+
+func (v *OAuthValidator) backChannelLogOutHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := r.ParseForm()
+		if err != nil {
+			log.Printf("Error parsing form: %v", err)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		tokenString := r.FormValue("logout_token")
+		if tokenString == "" {
+			log.Printf("logout_token is required")
+			http.Error(w, "logout_token is required", http.StatusBadRequest)
+			return
+		}
+
+		logoutToken, err := v.Verifier.Verify(r.Context(), tokenString)
+		if err != nil {
+			log.Printf("Error verifying logout token: %v", err)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		var claims map[string]interface{}
+		if err := logoutToken.Claims(&claims); err != nil {
+			log.Printf("Error parsing logout token claims: %v", err)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		if v.Config.DumpClaims {
+			log.Printf("Dump logout token claims: %+v", claims)
+		}
+
+		v.SessionStore.Delete(r.Context(), claims["sid"].(string))
+	}
+}
+
+func (v *OAuthValidator) validateAndGetClaimsIDToken(ctx context.Context, oauth2Token *oauth2.Token) (*map[string]interface{}, error) {
+	rawIDToken, ok := oauth2Token.Extra("id_token").(string)
+	if !ok {
+		return nil, fmt.Errorf("id_token is required")
+	}
+
+	idToken, err := v.Verifier.Verify(ctx, rawIDToken)
+	if err != nil {
+		return nil, fmt.Errorf("failed to verify id_token: %w", err)
+	}
+
+	claims := map[string]interface{}{}
+	if err := idToken.Claims(&claims); err != nil {
+		return nil, fmt.Errorf("failed to get user info: %w", err)
+	}
+
+	if v.Config.DumpClaims {
+		log.Printf("Dump ID token claims: %+v", claims)
+	}
+
+	return &claims, nil
+}
+
+func (v *OAuthValidator) verifyState(r *http.Request) error {
+	state := r.URL.Query().Get("state")
+
+	if _, err := v.StateStore.Get(r.Context(), state); err != nil {
+		return fmt.Errorf("invalid state: %v", err)
+	}
+
+	return nil
 }
